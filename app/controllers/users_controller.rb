366b226e202c41b201028448efef3a9b39109838
Bekzod Tursunov <amishopogolik@gmail.com>
2020-08-16 15:52:11 +0500

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,53 @@
+# (c) goodprogrammer.ru
+#
+# Контроллер, управляющий пользователями. Должен уметь:
+#
+#   1. Показывать страницу пользователя
+#   2. Создавать новых пользователей
+#   3. Позволять пользователю редактировать свою страницу
+#
 class UsersController < ApplicationController
+  # Это действие отзывается, когда пользователь заходит по адресу /users
+  def index
+    # Мы создаем массив из двух болванок пользователей. Для создания фейковой
+    # модели мы просто вызываем метод User.new, который создает модель, не
+    # записывая её в базу.
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Bekzod',
+        username: 'bekushka'
+      ),
+      User.new(id: 2, name: 'Farangiz', username: 'fara')
+    ]
+  end
 
+  def new
+  end
+
+  def edit
+  end
+
+  # Это действие отзывается, когда пользователь заходит по адресу /users/:id,
+  # например /users/1.
   def show
-    @hello = "Привет, Бекзодик!"
+    # Болванка пользователя
+    @user = User.new(
+      name: 'Bekzod',
+      username: 'bekushka'
+    )
+
+    # Болванка вопросов для пользователя
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('20.08.2020')),
+      Question.new(
+        text: 'В чем смысл жизни?', created_at: Date.parse('20.08.2020')
+      )
+    ]
+
+    # Болванка для нового вопроса
+    @new_question = Question.new
+
+    # Обратите внимание, пока ни одна из болванок не достается из базы
   end
 end
