711804921bc52c9bfb2fe21bf826ef8c957315f8
Bekzod Tursunov <amishopogolik@gmail.com>
2020-09-01 23:38:31 +0500

    correct version of validations to username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,19 @@
 require 'openssl'
 
 # Модель пользователя.
-class User < ActiveRecord::Base
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  VALID_USER_REGEX = /\A[a-zA-Z0-9_]+\Z/i
+class User < ApplicationRecord
 
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_USER_REGEX = /\A[\w]+\z/
+
   attr_accessor :password
 
-  has_many :questions
+  has_many :questions, dependent: :destroy
+
+  before_save :encrypt_password
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
@@ -23,19 +25,13 @@ class User < ActiveRecord::Base
   validates :email, format: { with: VALID_EMAIL_REGEX }
   validates :username, format: { with: VALID_USER_REGEX }
 
-  validates_confirmation_of :password
-
-  before_save :encrypt_password
+  validates :password, confirmation: true
 
   def encrypt_password
     if password.present?
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(
-          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
-        )
-      )
+      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST))
     end
   end
 
@@ -50,12 +46,10 @@ class User < ActiveRecord::Base
 
     hashed_password = User.hash_to_string(
       OpenSSL::PKCS5.pbkdf2_hmac(
-        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
-      )
+        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST)
     )
 
-    return user if user.password_hash == hashed_password
-
-    nil
+    return unless user.password_hash == hashed_password
+    user
   end
 end
