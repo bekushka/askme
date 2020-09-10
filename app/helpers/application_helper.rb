d2aea58c02f38d16f939bdf5e9e7939d57c205e4
Bekzod Tursunov <amishopogolik@gmail.com>
2020-09-10 05:22:21 +0500

    added new method of declinsion for words in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,31 @@ module ApplicationHelper
     end
   end
 
+  def question_counted(questions_count)
+    word_declination(questions_count, 'вопрос', 'вопроса', 'вопросов')
+  end
+
+  def answer_counted(answers_count)
+    word_declination(answers_count, 'ответ', 'ответа', 'ответов')
+  end
+
+  def word_declination(number, one, few, many)
+    if (11..14).include?(number % 100)
+      many
+    end
+
+    residue = number % 10
+
+    if residue == 1
+      one
+    elsif (2..4).include?(residue)
+      few
+    else
+      many
+    end
+  end
+
+
   # Хелпер, рисующий span тэг с иконкой из font-awesome
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
