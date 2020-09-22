92caf929ba2f65393850c755ab30fa454c562bf0
Bekzod Tursunov <amishopogolik@gmail.com>
2020-09-22 23:30:22 +0500

    add change color in user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_171323) do
+ActiveRecord::Schema.define(version: 2020_09_22_182521) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
